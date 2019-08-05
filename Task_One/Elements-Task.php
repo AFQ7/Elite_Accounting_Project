9e4fc36c24358299202a9b24d6f478ed5f2d9849
AFQ7 <alfarooq.elite@outlook.com>
2019-08-06 01:36:23 +0300

    Modify on Element-Task

--- Task_One/Elements-Task.php
+++ Task_One/Elements-Task.php
@@ -1,1 +1,16 @@
+<?php
 
+class inputElements{
+
+function InBox($LaBle, $InType) // $LaBle is for adding a Label befor befor any input Element
+// $InType is for chosing type of input Elemnt (text, password, radio, email, file, etc.) 
+{
+    echo "$LaBle"; echo "<br>";
+    print(" <input type=\"$InType\"> "); echo "<br>";
+}
+}
+
+$InputCall = new inputElements; 
+$InputCall->InBox("Username:", "text");
+
+?>
