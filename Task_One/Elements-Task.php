ae1e34bbb8e510251f186374302bd401e87532f8
Alfarooq Omar <53664402+AFQ7@users.noreply.github.com>
2019-08-14 10:06:27 +0300

    Modifying the code with new function

--- Task_One/Elements-Task.php
+++ Task_One/Elements-Task.php
@@ -1,46 +1,49 @@
 <?php
 
-/* Classes' name should starts with capital letter and preferred to be in camel case */
+//======================================================================
+// CLASS FOR ADDING INPUT ELEMENTS  
+//======================================================================
+
 class InputElements{
 
-    /*
-    *
-    * NOTES:
-    * 1- Function's name preferred to be short and in camel case.
-    * 2- Variables' names also preferred to be short and in camel case.
-    * 3- It is important to define the function whether it's public, private or protected.
-    * 4- It is much better to give initial values to all variables if possible.
-    * 5- Every function should return data, and should not echo or print anything.
-    * 6- HTML code returned from a function should not have <br>, CSS codes or any style. It should be pure HTML.
-    *
-    * VARIABLES:
-    * $txt      => is for adding a Label before any input element.
-    * $params   => is all the parameters to configure the input.
-    *
-    */
-    public function addInput($txt = null, $params = array()){
-        $out = "<div";
-        $divClass = $this->val('divClass', null, $params);
-        if($divClass != null){
-            $out.= " class=\"".$divClass."\"";
-        }
-        $out.= ">";
-        if($txt != null){
-            $out.= "<label>".$txt.":</label>";
-        }
-        $out.= "<input type=\"".$this->val('type', 'text', $params)."\">";
-        $out.= "</div>";
-        return $out;
-    }
+/*  Input Addtion function  */
+# First Input: $LableText is for adding a Label befor befor any input Element  
+# Second Input: $InputPrams is for chosing type of input Elemnt (text, password, etc.)
+# Output (Return): $OutputShown is for showing desired Input Element
+
+
+public function InBox($LableText = null, $InputPrams = array()) 
+{
+
+    $OutputShown = null;
     
-    private function val($index, $initialVal, $array){
-        return (array_key_exists($index, $array)) ? $array[$index]  : $initialVal;
-    }
+    if ($LableText != null) {$OutputShown .= "<label>".$LableText.": </label>";}
+    // The above code is to Show the Lable of the Input Element
+
+    $OutputShown .= "<input ";
+    $OutputShown .= $this-> FetchIndex($InputPrams);
+    $OutputShown .= ">";
+    return print("$OutputShown");;
 }
 
-$forms = new InputElements; 
-echo $forms->addInput("Username", array(
-    'type' => 'number'
+private function FetchIndex($TempArray = array())
+{
+    $RefrenceIndex = null;
+    foreach ($TempArray as $key => $value)
+    {
+        $RefrenceIndex .= "$key=";
+        $RefrenceIndex .= "\"$value\"";
+        $RefrenceIndex .= " ";
+    }
+    return $RefrenceIndex;
+}
+}
+
+$InputCall = new InputElements;
+$InputCall->InBox("Username" ,array(
+    'type' => 'text',
+    'name' => 'FirstName',
+    'placeholder' => 'Enter Username'
 ));
 
 ?>
