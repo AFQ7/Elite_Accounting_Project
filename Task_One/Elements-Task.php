2d2c425ae277460341e45423d49ef8a0948b6d4e
Alfarooq Omar <53664402+AFQ7@users.noreply.github.com>
2019-09-02 12:13:31 +0300

    Major Updates for the Class and Functions
    The changes have been done to the main detection function and take in concern the probability of any input type.

--- Task_One/Elements-Task.php
+++ Task_One/Elements-Task.php
@@ -4,7 +4,8 @@
 // CLASS FOR ADDING INPUT ELEMENTS  
 //======================================================================
 
-class InputElements{
+class InputElements
+{
 
 /*  Input Addtion function  */
 # First Input: $LableText is for adding a Label befor befor any input Element
@@ -14,25 +15,41 @@ class InputElements{
 # Output (Return): $OutputShown is for showing the desired Input Element
 
 
-public function addInput($LableText = null, $NewLine = false , $InputPrams = array(), $InputValue = array()) 
+public function addInput($LableText = null, $NewLine = false , $InputPrams = array(), $InputValue = array(), $valueLable =null) 
 {
-    $this->showElment($this->processInput($LableText, $NewLine, $InputPrams, $InputValue ));
+    $this->showElment($this->processInput($LableText, $NewLine, $InputPrams, $InputValue, $valueLable ));
 }
 
-private function processInput($LableTexT = null, $NewLinE = false , $InputPramS = array(), $InputValueS = array()) 
+private function processInput($LableTexT = null, $NewLinE = false , $InputPramS = array(), $InputValueS = array(), $ValueLable =null) 
 {
     $OutputShown = "<div";
 
+    $OutputShown .= $this->divClassFetch($InputPramS);
     $OutputShown .= $this-> labelFetch($LableTexT, $NewLinE);
-    $OutputShown .= $this-> inputArraylFetch($InputPramS, $InputValueS);
-    //if ($LableText != null) {$OutputShown .= "<label>".$LableText."</label>";}
-    // The above code is to Show the Lable of the Input Element
-
-    //$OutputShown .= "<input ";
-    //$OutputShown .= $this-> FetchIndex($InputPrams);
+    $OutputShown .= $this-> inArraylFetch($InputPramS, $InputValueS , $ValueLable);
+    
     $OutputShown .= "</div>";
     return $OutputShown;
-}
+
+} // end of processInput function
+
+private function divClassFetch ($TempArray = array()){
+    $FetchShown = null;
+    if ($TempArray != null)
+    {
+        foreach ($TempArray as $key => $value)
+        {
+            if (current($this->checkInputValues($key,$value)) == null )
+            {
+                if( key($this->checkInputValues($key,$value)) == 'list' || 'name'){
+                    $FetchShown .= " class=";
+                    $FetchShown .= "\"$value\">";
+                }
+            }
+        }
+    return $FetchShown;
+    } else { return null; }
+} // end of labelFetch function
 
 private function labelFetch ($CommentText, $EndLine){
     $FetchShown = null;
@@ -44,100 +61,8 @@ private function labelFetch ($CommentText, $EndLine){
         } else {$FetchShown .= "<label>".$CommentText."</label>";}
         return $FetchShown;
     } else { return null; }
-}
+} // end of labelFetch function
 
-private function inputArraylFetch ($TempArray = array(), $TempValueArray= array())
-{
-    $typeFlag =null;
-    $nameFlag =null;
-    $identityFlag =null;
-    $typeFlag =null;
-    $FetchShown=null;
-    $correctValue = false;
-    $vlaueArrayCount = count($TempValueArray);
-    $recordArray = array();
-    $valueArrayFlag =true;
-
-    if ($TempArray != null)
-    {
-        foreach ($TempArray as $key => $value)
-        {
-            if ($this->checkNameVlaue($key) == true )
-            {
-                $nameFlag .= "name=";
-                $nameFlag .= "\"$value\" ";
-                $correctValue = true;
-            } elseif($this->checkTypeValue($key) == true) 
-            {
-                if ($vlaueArrayCount == 0)
-                {
-                    $typeFlag .= "type=";
-                    $typeFlag .= "\"$value\" ";
-                    $correctValue = true;
-                } elseif ($vlaueArrayCount > 0)
-                {
-                    $recordArray = $this->inputValueFetch($TempValueArray);
-                    for ($i = 0; $i < $vlaueArrayCount; $i++)
-                    {
-                        $typeFlag .= "type=";
-                        $typeFlag .= "\"$value\" ";
-                        $typeFlag .= "value=";
-                        $typeFlag .= $recordArray[$i];
-                        $FetchShown .= "<input ".$nameFlag.$typeFlag.$identityFlag."> ";
-                        $FetchShown .= str_replace('"', '', $recordArray[$i]);;
-                        $FetchShown .= "\n";
-                    } return $FetchShown;
-                    $valueArrayFlag = false;
-                    $correctValue = true;
-                }
-            }elseif($this->checkListValue($key) == true) 
-            {
-                if ($vlaueArrayCount == 0)
-                {
-                    $typeFlag .= "list=";
-                    $typeFlag .= "\"$value\"> ";
-                    $typeFlag .= "<datalist id=";
-                    $typeFlag .= "\"$value\"> ";
-                    $typeFlag .= "<option value=";
-                    $typeFlag .= "\"Intail-Value\" ";
-                    $typeFlag .= "> </datalist";
-                    $correctValue = true;
-                } elseif ($vlaueArrayCount > 0)
-                {
-                    $recordArray = $this->inputValueFetch($TempValueArray);
-                    $FetchShown .= "<input ";
-                    $FetchShown .= "list=";
-                    $FetchShown .= "\"$value\" ";
-                    $FetchShown .= "".$nameFlag.$identityFlag.">";
-                    $FetchShown .= "<datalist id=";
-                    $FetchShown .= "\"$value\"> ";
-                    for ($i = 0; $i < $vlaueArrayCount; $i++)
-                    {
-                        $FetchShown .= "<option value=";
-                        $FetchShown .= $recordArray[$i];
-                        $FetchShown .= "> \n";
-                    } 
-                    return $FetchShown;
-                    $valueArrayFlag = false;
-                    $correctValue = true;
-                }
-            }
-
-        } 
-        if($correctValue != true)
-        {
-            $nameFlag .= "name=";
-                $nameFlag .= "\"intialID\" ";
-                $typeFlag .= "type=";
-                $typeFlag .= "\"text\" ";
-        }
-        if($valueArrayFlag == true)
-        {
-            $FetchShown .= "<input ".$nameFlag.$typeFlag.$identityFlag.">";
-            return $FetchShown;
-        }
-    } else { return null; }
-}
 
 private function inputValueFetch ($TempArray = array())
 {
@@ -155,25 +80,138 @@ private function inputValueFetch ($TempArray = array())
             } array_push($FetchShownArray, $inputValueFlag);
         } return $FetchShownArray;
     } else { return null; }
-}
+} // end of inputValueFetch function
 
-private function checkListValue ($inText){
-    $nameShown =null;
-    similar_text("list", $inText, $simPercent);
-    if($simPercent >= 40.0)
-    {
-        return true;
-    }else {return false;}
-}
 
-private function checkNameVlaue ($inText){
-    $nameShown =null;
-    similar_text("name", $inText, $simPercent);
-    if($simPercent >= 40.0)
+private function inArraylFetch ($TempArray = array(), $TempValueArray= array(), $ValueLable =null)
+{
+    $intialtypeFlag =null;
+    $listFlag =null;
+    $inputFlag =null;
+    $typeFlag =null;
+    
+    $FetchShown=null;
+
+    $correctValue = false;
+
+    $ArrayValueCount = count($TempValueArray);
+    $valueArrayFlag =true;
+
+    $recordArray = array();
+    
+    if ($TempArray != null)
     {
-        return true;
-    }else {return false;}
-}
+        foreach ($TempArray as $key => $value)
+        {
+            if (current($this->checkInputValues($key,$value)) == null )
+            {
+                if( $key == 'list' )
+                {
+                    $listFlag .= "".key($this->checkInputValues($key,$value))."";
+                    $listFlag .= "=\"$value\"> ";
+                    $listFlag .= "<datalist id=";
+                    $listFlag .= "\"$value\"> ";
+                    if ($ArrayValueCount == 0)
+                    {
+                        $listFlag .= "<option value=";
+                        $listFlag .= "\"Intail-Value\" ";
+                        $listFlag .= "> </datalist";
+                        $correctValue = true;
+                    } elseif ($ArrayValueCount > 0)
+                    {
+                        $recordArray = $this->inputValueFetch($TempValueArray);
+                        for ($i = 0; $i < $ArrayValueCount; $i++)
+                        {
+                            $listFlag .= "<option value=";
+                            $listFlag .= $recordArray[$i];
+                            $listFlag .= ">";
+                        } 
+                        $listFlag .= "</datalist";
+                        $valueArrayFlag = true;
+                        $correctValue = true;
+                    }
+                    
+                } else {
+                    $inputFlag .= "".key($this->checkInputValues($key,$value))." ";
+                    if($value != null){
+                        $inputFlag .= "=\"$value\" ";
+                    }
+                    
+                    $valueArrayFlag = true;
+                    $correctValue = true;
+                }
+            } elseif (current($this->checkInputValues($key,$value)) != null )
+            {
+                if ($ArrayValueCount == 0)
+                {
+                    $typeFlag .= "".key($this->checkInputValues($key,$value))."";
+                    $typeFlag .= "="."\"".current($this->checkInputValues($key,$value))."\" ";
+                    $valueArrayFlag = true;
+                    $correctValue = true;
+                } elseif ($ArrayValueCount > 0)
+                {
+                    $recordArray = $this->inputValueFetch($TempValueArray);
+                    for ($i = 0; $i < $ArrayValueCount; $i++)
+                    {
+                        $typeFlag = null;
+                        $typeFlag .= "".key($this->checkInputValues($key,$value))."";
+                        $typeFlag .= "="."\"".current($this->checkInputValues($key,$value))."\" ";
+                        $typeFlag .= "value=";
+                        $typeFlag .= $recordArray[$i];
+                        $FetchShown .= "<input ".$inputFlag.$typeFlag."> ";
+                        if($ValueLable == true)
+                        {$FetchShown .= str_replace('"', '', $recordArray[$i]);}
+                        $FetchShown .= "\n";
+                    } return $FetchShown;
+                    $valueArrayFlag = false;
+                    $correctValue = true;
+                }
+            }
+
+        } 
+        if($correctValue != true)
+        {
+            $intialtypeFlag .= "name=";
+            $intialtypeFlag .= "\"intialID\" ";
+            $intialtypeFlag .= "type=";
+            $intialtypeFlag .= "\"text\" ";
+            $FetchShown .= "<input ".$intialtypeFlag.">";
+            return $FetchShown;
+        } elseif($valueArrayFlag == true)
+        {
+            $FetchShown .= "<input ".$inputFlag.$typeFlag.$listFlag.">";
+            return $FetchShown;
+        }
+    } else { return null; }
+} // end of inputArraylFetch function
+
+
+/*  Text Check and Similarity Functions  */
+# Single Input: For check and process the typed and input text with main index
+
+private function checkInputValues ($inKeyText, $inValText)
+{
+    $returnIndex = array();
+    foreach ($this->InputAttribute as $key => $value)
+    {
+        similar_text($key, $inKeyText, $simKeyPercent);
+        if($simKeyPercent >= 80.0)
+        {
+            if (is_array($value)) 
+            {
+                foreach ($value as $inside)
+                {
+                    similar_text($inside, $inValText, $simValuePercent);
+                    if ($simValuePercent >= 80.0)
+                    {
+                        $returnIndex[$key]=$inside;
+                    }
+                }
+            } else { $returnIndex[$key]=null; }  
+        }
+    }
+    return $returnIndex; 
+}  // end of checkInputValues function
 
 private function checkValueText ($inText){
     $nameShown =null;
@@ -182,31 +220,61 @@ private function checkValueText ($inText){
     {
         return true;
     }else {return false;}
-}
+} // end of checkValueText function
 
-private function checkTypeValue ($inText){
-    $nameShown =null;
-    similar_text("type", $inText, $simPercent);
-    if($simPercent >= 40.0)
-    {
-        return true;
-    }else {return false;}
-}
+
+
+/*  Show and Print to Screen Functions  */
+# Single Input: For print the typed and input text on screen with desirable form
 
 private function showElment($InVariable)
 {
     echo nl2br($InVariable);
-}
-}
+} // end of showElment function
 
-$InputCall = new InputElements;
-$InputCall->addInput("Username", true , array(
+private $InputAttribute = array (
+    'accept' => null,
+    'alt' => null,
+    'autofocus' => null,
+    'autocomplete' => array( 'on', 'off'),
+    'checked' => null,
+    'dirname' => null,
+    'disabled' => null,
+    'form' => null,
+    'formaction' => null,
+    'formenctype' => null,
+    'formmethod' => null,
+    'formnovalidate' => null,
+    'formtarget' => null,
+    'height' => null,
+    'list' => null,
+    'max' => null, 
+    'min' => null,
+    'maxlength' => null,
+    'multiple' => null,
+    'name' => null,
+    'pattern' => null,
+    'placeholder' => null,
+    'readonly' => null,
+    'size' => null,
+    'src' => null,
+    'step' => null,
+    'type' => array('button', 'checkbox', 'color', 'date', 'datetime-local', 'email', 'file', 'hidden', 'image', 'month', 'number', 'password', 'radio', 'range', 'search', 'submit', 'tel', 'text', 'time', 'url'),
+    'width' => null 
+);
+
+} // end of InputElements class
+
+
+// Main Program Process and Run for using the function
+/*$InputCall = new InputElements;
+$InputCall->addInput("Select Gender Type:", true , array(
 'name' => 'Gender',
-'list' => 'Gender'
+'type' => 'radio'
+
 ), array(
     'value1' => 'Male',
-    'value2' => 'Female',
-    'value3' => 'Other'
-));
+    'value2' => 'Female'
+), false);*/
 
 ?>
